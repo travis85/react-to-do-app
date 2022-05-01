b6a53afc63483a86b0f5e928a09ae7f296a5016a
travis flake <tflake83@gmail.com>
2022-05-01 10:42:49 -0400

    added props

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,14 +4,13 @@ import { auth } from '../utils/firebase'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 
 
-export default function SignIn() {
+export default function SignIn(props) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     async function handleSignIn(){
-       const res = await signInWithEmailAndPassword(auth,email,password)
-        
-       console.log(res)
+      const res = await signInWithEmailAndPassword(auth,email,password)
+      props.handleUserRecieved(res.user)
     }
     function updateEmail(event){
         setEmail(event.target.value)
