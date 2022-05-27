c94af9580f941f5b57398a7b60abca63a8466804
travis flake <tflake83@gmail.com>
2022-05-27 13:16:31 -0400

    signIn error message

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,25 +5,44 @@ import { signInWithEmailAndPassword } from 'firebase/auth'
 
 
 export default function SignIn(props) {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
-    async function handleSignIn(){
-      const res = await signInWithEmailAndPassword(auth,email,password)
-      props.handleUserRecieved(res.user)
+  
+  function clearError() {
+    setTimeout(() => {
+      setErrorMessage('')
+    },3000)
+  }
+
+  
+  async function handleSignIn() {
+    //loading true
+
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password)
+      props.handleUserRecieved(res.user) 
+    } catch (error) {
+      setErrorMessage('Sign In Error')
+      clearError()
     }
-    function updateEmail(event){
-        setEmail(event.target.value)
-    }
-    function updatePassword(event){
-        setPassword(event.target.value)
-    }
+    
+  }
+
+  function updateEmail(event){
+      setEmail(event.target.value)
+  }
+  function updatePassword(event){
+      setPassword(event.target.value)
+  }
 
   return (
-    <div>
-        <input className='bg-gray-400 border-white border-solid px-4 py-2 placeholder-white' type='text' placeholder='Email' value={ email } onChange={ updateEmail }></input>
-        <input className='bg-gray-400 border-white border-solid px-4 py-2 m-4 placeholder-white' type='password' placeholder='Password' value={ password } onChange={ updatePassword }></input>
-        <button className='text-white hover:text-slate-400' onClick={()=> (handleSignIn())}>Sign In</button>
+    <div className='grid grid-cols-1 place-items-center '>
+        <input className='bg-gray-400 border-white border-solid px-4 py-2 placeholder-white mb-2' type='text' placeholder='Email' value={ email } onChange={ updateEmail }></input>
+        <input className='bg-gray-400 border-white border-solid px-4 py-2  placeholder-white' type='password' placeholder='Password' value={ password } onChange={ updatePassword }></input>
+      <button className='text-white hover:text-slate-400' onClick={() => (handleSignIn())}>Sign In</button>
+      <p className='text-red-500'>{ errorMessage }</p>
 
     </div>
   )
